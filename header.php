34ba296c5421d264e2ccca7b3b7c51bdf75c9ffd
mpstenson <matt.stenson@gmail.com>
2015-07-07 22:33:36 -0500

    added page template and other fixes to overall div structure. Split header out into the header.php file

---
+++ header.php
@@ -0,0 +1,39 @@
+<head>
+    <title></title>
+    <link rel="stylesheet" href="<?php echo get_bloginfo('template_url'); ?>/css/uikit.min.css" />
+    <link rel="stylesheet" href="<?php echo get_bloginfo('template_url'); ?>/style.css" />
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.4/jquery.min.js"></script>
+    <script src="<?php echo get_bloginfo('template_url'); ?>/js/uikit.min.js"></script>
+    <script src="<?php echo get_bloginfo('template_url'); ?>/js/components/parallax.min.js"></script>
+    <script src="<?php echo get_bloginfo('template_url'); ?>/js/components/sticky.min.js"></script>
+</head>
+
+
+<body>
+
+<div class="uk-width-medium-1-2 " style="background-color:#fff; padding-bottom:4px; z-index:1000" data-uk-sticky >
+<?php
+
+$defaults = array(
+'theme_location'  => '',
+'menu'            => '',
+'container'       => 'nav',
+'container_class' => '',
+'container_id'    => '',
+'menu_class'      => 'menu',
+'menu_id'         => '',
+'echo'            => true,
+'fallback_cb'     => 'wp_page_menu',
+'before'          => '',
+'after'           => '',
+'link_before'     => '',
+'link_after'      => '',
+'items_wrap'      => '<ul>%3$s</ul>',
+'depth'           => 0,
+'walker'          => ''
+);
+
+wp_nav_menu( $defaults );
+
+?>
+</div>
--- page.php
+++ page.php
@@ -3,8 +3,6 @@
 <?php get_header(); ?>
 
 
-
-
         <div class="uk-grid">
           <?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
             <?php if (has_post_thumbnail( $post->ID ) ): ?>
@@ -18,14 +16,9 @@
                 <div class="uk-width-1-10"></div>
                 <div class="uk-width-8-10">
                   <h1 data-uk-parallax="{opacity:0, viewport: -.5, y:0}" class="uk-align-center"><a title="Permanent link to <?php the_title_attribute(); ?>" rel="bookmark" href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h1>
-                  <h2 data-uk-parallax="{opacity:0, viewport: -.5, y:100}"><?php echo the_excerpt(); ?> </h2>
-                  <!--<div class="uk-width-2-10 uk-align-center">Read More</div>-->
-                  <a href="#post-<?php the_ID(); ?>" data-uk-parallax="{opacity:0, viewport: -.5, y:200}" class="uk-icon-angle-double-down uk-align-center" style="font-size:40px; text-align:center; color:#000" data-uk-smooth-scroll="{offset: 50}"></a>
-                  <div class="uk-width-1-10"></div></div></div></div>
+                  <div class="uk-width-1-10"></div>
             <div class="uk-width-2-10"></div><div class="uk-width-6-10">
               <article id="post-<?php the_ID(); ?>">
-
-
 
                 		<?php
                 			the_content(__('Continued&hellip;'));
@@ -50,9 +43,8 @@
 
                 	<?php edit_post_link(); ?>
 
-
             </article>
-            </div>
+        </div>
             <div class="uk-width-2-10"></div>
             <?php endwhile; ?>
             <!-- post navigation -->
