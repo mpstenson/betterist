31161286e9b9f918e4b39ce7a7fe354743287470
mpstenson <matt.stenson@gmail.com>
2015-11-04 20:50:55 -0600

    New page style

--- page.php
+++ page.php
@@ -1,28 +1,24 @@
 <!DOCTYPE html>
 <html>
 <?php get_header(); ?>
-
-
-        <div class="uk-grid" id="content-container">
+        <div class="bet-grid" id="content-container">
           <?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
             <?php if (has_post_thumbnail( $post->ID ) ): ?>
               <?php $image = wp_get_attachment_image_src( get_post_thumbnail_id( $post->ID ), 'single-post-thumbnail' ); ?>
-              <div class="uk-width-1-1" style="background-image: url('<?php echo $image[0]; ?>')" data-uk-parallax="{bg: '-400'}">
-                <div class="uk-grid">
-                  <div class="uk-width-1-10"></div>
-                  <div class="uk-width-8-10">
-                    <h1 data-uk-parallax="{opacity:0, viewport: -.5, y:0}" class="uk-align-center page-header"><a title="Permanent link to <?php the_title_attribute(); ?>" rel="bookmark" href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h1>
-                  </div>
-                  <div class="uk-width-1-10"></div>
-                </div>
-</div>
+              <div class="bet-col-1-1 header-container page-header" style="background-image:linear-gradient(rgba(255, 255, 255, 0.9), rgba(255, 255, 255, 0)),  url('<?php echo $image[0]; ?>')">
+
 <?php endif; ?>
 
 
-
-                  <div class="uk-grid">
-            <div class="uk-width-medium-2-10"></div><div class="uk-width-medium-6-10">
+              <div class="bet-grid">
+                <div class="bet-col-1-10"></div>
+                <div class="bet-col-8-10">
+                  <h1 class="uk-align-center"><a title="Permanent link to <?php the_title_attribute(); ?>" rel="bookmark" href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h1>
+                  <div class="bet-col-1-10"></div></div></div></div>
+            <div class="bet-col-2-10 bet-responsive-hide"></div><div class="bet-col-6-10 bet-responsive-stretch">
               <article id="post-<?php the_ID(); ?>">
+
+
 
                 		<?php
                 			the_content(__('Continued&hellip;'));
@@ -33,7 +29,7 @@
 
                 	<?php the_tags(); ?>
 
-                	<?php the_category(); ?>
+                	Categories: <?php the_category(); ?>
 
                 	<address class="vcard author">
                 		<span class="n"><?php the_author(); ?></span>
@@ -43,14 +39,13 @@
 
                 	<a href="<?php trackback_url(); ?>"><?php _e('Trackback URL'); ?></a>
 
-                	<?php comments_popup_link(); ?>
-
+                	<?php comments_template(); ?>
                 	<?php edit_post_link(); ?>
 
+
             </article>
-        </div>
-            <div class="uk-width-medium-2-10"></div>
-          </div>
+            </div>
+            <div class="bet-col-2-10 bet-responsive-hide"></div>
             <?php endwhile; ?>
             <!-- post navigation -->
           <?php else: ?>
