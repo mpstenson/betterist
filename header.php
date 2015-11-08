b52740dcf604c04e600dea6442919295906ec6ef
mpstenson <matt.stenson@gmail.com>
2015-11-07 22:52:41 -0600

    Working on passing the theme checker. Added post navigation

--- header.php
+++ header.php
@@ -21,9 +21,10 @@
 <meta property="og:description" content="<?php bloginfo('description'); ?>" />
 <meta property="og:type" content="website" />
 <meta property="og:image" content="logo.jpg" /> <?php } ?>
+<?php wp_head(); ?>
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.4.0/css/font-awesome.min.css">
-    <link rel="stylesheet" href="<?php echo get_bloginfo('template_url'); ?>/css/normalize.css">
-    <link rel="stylesheet" href="<?php echo get_bloginfo('template_url'); ?>/style.css" />
+    <link rel="stylesheet" href="<?php echo get_template_directory_uri(); ?>/css/normalize.css">
+    <link rel="stylesheet" href="<?php echo get_template_directory_uri(); ?>/style.css" />
 
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.4/jquery.min.js"></script>
     <script>
@@ -46,10 +47,10 @@
 </head>
 
 
-<body>
+<body <?php body_class( $class ); ?>>
   <div style="position:fixed; width:100%; background-color:#fff; ">
 <div class="bet-grid" style="height:50px;">
-<div class="bet-col-5-10 bet-responsive-show" id="site-title"><img src="<?php echo get_bloginfo('template_url'); ?>/images/LogoWide.png" style="height:50px; float:left;"></div>
+<div class="bet-col-5-10 bet-responsive-show" id="site-title"><img src="<?php echo get_template_directory_uri()  ?>/images/LogoWide.png" style="height:50px; float:left;"></div>
 <div class="bet-col-5-10" style="padding-bottom:4px; z-index:1000" >
 <i class="fa fa-bars bet-responsive-show" id="mobile-menu-hamburger"></i>
 <?php
@@ -103,6 +104,6 @@ wp_nav_menu( $mobile_defaults );
 
 ?>
 
-<div class="bet-col-5-10 bet-responsive-hide" id="site-title"><img src="<?php echo get_bloginfo('template_url'); ?>/images/LogoWide.png" style="height:50px; float:right;"></div>
+<div class="bet-col-5-10 bet-responsive-hide" id="site-title"><img src="<?php echo get_template_directory_uri();  ?>/images/LogoWide.png" style="height:50px; float:right;"></div>
 </div>
 </div>
--- index.php
+++ index.php
@@ -2,62 +2,78 @@
 <html>
 <meta name=viewport content="width=device-width, initial-scale=1">
 <?php get_header(); ?>
-        <div class="bet-grid" id="content-container">
-          <?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
-            <?php if (has_post_thumbnail( $post->ID ) ): ?>
-              <?php $image = wp_get_attachment_image_src( get_post_thumbnail_id( $post->ID ), 'single-post-thumbnail' ); ?>
-              <div class="bet-col-1-1 header-container" style="background-image:linear-gradient(rgba(255, 255, 255, 0.9), rgba(255, 255, 255, 0)),  url('<?php echo $image[0]; ?>')">
-
-<?php endif; ?>
-
-
+  <div class="bet-grid" id="content-container">
+    <?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
+      <?php if (has_post_thumbnail( $post->ID ) ): ?>
+        <?php $image = wp_get_attachment_image_src( get_post_thumbnail_id( $post->ID ), 'single-post-thumbnail' ); ?>
+          <div class="bet-col-1-1 header-container" style="background-image:linear-gradient(rgba(255, 255, 255, 0.9), rgba(255, 255, 255, 0)),  url('<?php echo $image[0]; ?>')">
+            <?php endif; ?>
               <div class="bet-grid">
                 <div class="bet-col-1-10"></div>
                 <div class="bet-col-8-10">
-                  <h1 data-uk-parallax="{opacity:0, viewport: -.5, y:0}" class="uk-align-center"><a title="Permanent link to <?php the_title_attribute(); ?>" rel="bookmark" href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h1>
-                  <h2 data-uk-parallax="{opacity:0, viewport: -.5, y:100}"><?php echo the_excerpt(); ?> </h2>
+                  <h1 data-uk-parallax="{opacity:0, viewport: -.5, y:0}" class="uk-align-center">
+                    <a title="Permanent link to <?php the_title_attribute(); ?>" rel="bookmark" href="<?php the_permalink(); ?>">
+                      <?php the_title(); ?>
+                    </a>
+                  </h1>
+                  <h2 data-uk-parallax="{opacity:0, viewport: -.5, y:100}">
+                    <?php echo the_excerpt(); ?>
+                  </h2>
                   <!--<div class="uk-width-2-10 uk-align-center">Read More</div>-->
-                  <a href="#post-<?php the_ID(); ?>"  class="fa fa-angle-double-down" style="font-size:40px; text-align:center; color:#000; text-decoration:none; width:100%"></a>
-                  <div class="bet-col-1-10"></div></div></div></div>
-            <div class="bet-col-2-10 bet-responsive-hide"></div><div class="bet-col-6-10 bet-responsive-stretch">
-              <article id="post-<?php the_ID(); ?>">
-
-
-
-                		<?php
+                  <a href="#post-<?php the_ID(); ?>" class="fa fa-angle-double-down" style="font-size:40px; text-align:center; color:#000; text-decoration:none; width:100%"></a>
+                  <div class="bet-col-1-10"></div>
+                </div>
+              </div>
+          </div>
+          <div class="bet-col-2-10 bet-responsive-hide"></div>
+          <div class="bet-col-6-10 bet-responsive-stretch">
+            <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+              <?php
                 			the_content(__('Continued&hellip;'));
                 			wp_link_pages();
 
                 		?>
+                <?php the_tags(); ?>
+                  Categories:
+                  <?php the_category(); ?>
+                    <address class="vcard author">
+                      <span class="n">
+                        <?php the_author(); ?>
+                      </span>
+                    </address>
 
+                    <abbr class="published" title="<?php the_time('Y-m-dTH:i'); ?>">
+                      <?php the_date(); ?> @
+                        <?php the_time(); ?>
+                    </abbr>
 
-                	<?php the_tags(); ?>
+                    <a href="<?php trackback_url(); ?>">
+                      <?php _e('Trackback URL'); ?>
+                    </a>
 
-                	Categories: <?php the_category(); ?>
+                    <?php comments_popup_link(); ?>
 
-                	<address class="vcard author">
-                		<span class="n"><?php the_author(); ?></span>
-                	</address>
-
-                	<abbr class="published" title="<?php the_time('Y-m-dTH:i'); ?>"><?php the_date(); ?> @ <?php the_time(); ?></abbr>
-
-                	<a href="<?php trackback_url(); ?>"><?php _e('Trackback URL'); ?></a>
-
-                	<?php comments_popup_link(); ?>
-
-                	<?php edit_post_link(); ?>
+                      <?php edit_post_link(); ?>
 
 
             </article>
+          </div>
+          <div class="bet-col-2-10 bet-responsive-hide"></div>
+          <div class="bet-grid">
+            <div class="bet-col-1-1">
+              <div class="post-navigation"><?php posts_nav_link(); ?></div>
             </div>
-            <div class="bet-col-2-10 bet-responsive-hide"></div>
-            <?php endwhile; ?>
+          </div>
+          <?php endwhile; ?>
             <!-- post navigation -->
-          <?php else: ?>
-            <!-- no posts found -->
-          <?php endif; ?>
 
-        </div>
-        <?php get_footer(); ?>
+            <?php else: ?>
+              <!-- no posts found -->
+              <?php endif; ?>
+
+  </div>
+
+  <?php get_footer(); ?>
     </body>
+
 </html>
