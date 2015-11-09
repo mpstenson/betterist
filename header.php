e4557d692519d59df06bf0ba7ce6d44fc034acb5
mpstenson <matt.stenson@gmail.com>
2015-11-08 19:35:52 -0600

    Fix broken opening html tag

--- header.php
+++ header.php
@@ -1,5 +1,5 @@
 <!DOCTYPE html>
-<html <?php language_attributes(); ?>
+<html <?php language_attributes(); ?>>
 <meta name=viewport content="width=device-width, initial-scale=1">
 <head>
     <title><?php wp_title(''); ?>
